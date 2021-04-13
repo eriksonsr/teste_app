16c02ec6cc9430df7b652a643ea3b12194a6e242
Erikson Reppetto <erikson.souza@gmail.com>
2021-04-12 23:22:57 -0300

    Cadastrando usuário na api

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -11,4 +11,27 @@ class UsuariosController extends Controller
 	{
 		return json_encode(Usuarios::orderBy('nome')->get());
 	}
+
+	public function Cadastrar(Request $request)
+	{
+		$dados = [
+			'nome' => $request->input('nome'),
+			'email' => $request->input('email'),
+			'senha' => $request->input('senha'),
+			'telefone' => $request->input('telefone')
+		];
+
+		$usuario = new Usuarios();
+		$usuario->nome = $request->input('nome');
+		$usuario->email = $request->input('email');
+		$usuario->senha = $request->input('senha');
+		$usuario->telefone = $request->input('telefone');
+
+		try {
+			$usuario->save();
+			return json_encode(['status' => 'Sucesso', 'msg' => 'Usuário cadastrado com sucesso!']);
+		} catch (Exception $e) {
+			return json_encode(['status' => 'Erro', 'msg' => 'Ocorreu um erro ao cadastrar usuário.']);
+		}
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('listar', 'UsuariosController@ListarJson');
+Route::post('cadastrar', 'UsuariosController@cadastrar');
